6b23cff003084a994db6f29b61b8ca4e1bfe2f90
shagors <shajjadshagor@gmail.com>
2022-02-26 20:27:37 +0600

    logo and country name add. game name shown

--- js/app.js
+++ js/app.js
@@ -12,7 +12,20 @@ const searchFood = () => {
 
 const displaySearchResult = countrys =>{
     const searchResult = document.getElementById('search-result');
-    for(const country of countrys){
+    countrys.forEach(country =>{
         console.log(country);
-    }
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${country.strBadge}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${country.strSport}</h4>
+                    <h6>${country.strCountry}</h6>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
